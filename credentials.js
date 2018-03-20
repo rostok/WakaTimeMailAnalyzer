c0bcd511a52dd1698f228c7760323d6bcf826f66
rostok <rostok@3e.pl>
2018-03-20 23:57:43 +0100

    initial commit

---
+++ credentials.js
@@ -0,0 +1,6 @@
+module.exports = {
+    inbox: '<imap inbox folder>',
+    user: '<mailbox name>',
+    password: '<your password>',
+    host: '<imap server address>'
+}
---
+++ index.js
@@ -0,0 +1,153 @@
+var math = require('mathjs');
+var imaps = require('imap-simple');
+var fs = require('fs');
+
+
+var credentials = require('./credentials');
+
+var config = {
+  inbox: credentials.inbox, 
+  filterMinimalHours: 10,
+  imap: {
+    user: credentials.user, 
+    password: credentials.password, 
+    host: credentials.host, 
+    port: 994,
+    tls: true,
+    authTimeout: 3000
+  }
+};
+
+// this will hold projects, single project has 'total' property with total number of hours and week-end dates properties with hours spend on that weeks  
+var projects = {};
+
+/// silly little function
+function betw(src, beg, end) {
+  src = src.split(beg);
+  if (src.length <= 1) return src[0];
+  src.shift();
+  src = src.join("beg");
+  src = src.split(end);
+  return src[0];
+}
+
+console.log("connecting.");
+imaps.connect(config).then(function (connection) {
+
+  return connection.openBox(config.inbox).then(function () {
+    console.log("connected.");
+    var searchCriteria = [
+      ['SUBJECT', 'WakaTime'],
+      ['!SUBJECT', 'no coding activity']
+    ];
+
+    var fetchOptions = {
+      bodies: ['HEADER', 'TEXT'],
+      markSeen: false
+    };
+
+    return connection.search(searchCriteria, fetchOptions).then(function (results) {
+      console.log("searching.");
+      results.map(function (res) {
+        var s = res.parts.filter(function (part) {
+          return part.which === 'HEADER';
+        })[0].body.subject[0];
+        var t = res.parts.filter(function (part) {
+          return part.which === 'TEXT';
+        })[0].body;
+
+        var w = s.split("until").pop().trim();
+        p = betw(t, 'Projects:', 'Languages').trim();
+        p = p.split("\n");
+        p.map(pr => {
+          pr = pr.trim();
+          var d = [];
+          if (pr.indexOf("\t") != -1) {
+            d = pr.split("\t");
+          } else
+          if (pr.indexOf(" : ") != -1) {
+            d = pr.split(" : ");
+          } else
+            return;
+          d[0] = d[0].replace("'", "");
+          d[0] = d[0].replace('"', "");
+          var q = d[1];
+          q = q.replace("hrs", "*60*60+");
+          q = q.replace("hr", "*60*60+");
+          q = q.replace("mins", "*60+");
+          q = q.replace("min", "*60+");
+          q = q.replace("secs", "+");
+          q = q.replace("sec", "+");
+          q += "0";
+          d[1] = math.eval(q) / 3600;
+          if (typeof projects[d[0]] === 'undefined') projects[d[0]] = {
+            total: 0
+          };
+          projects[d[0]].total += d[1];
+          projects[d[0]][w] = d[1];
+        })
+      });
+    }).then(() => {
+      console.log("accumulating.");
+      //console.log(projects);
+      var weeks = Object.keys(projects);
+      weeks = weeks.reduce((arr, val) => {
+        return arr.concat(Object.keys(projects[val]));
+      }, []);
+      weeks = weeks.filter((v, i, a) => a.indexOf(v) === i);
+      weeks = weeks.filter(v => v != 'total').sort();
+
+      html = `<html>
+      <head>
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <script type="text/javascript">
+          google.charts.load('current', {'packages':['corechart']});
+          google.charts.setOnLoadCallback(drawChart);
+    
+          function drawChart() {
+            var data = google.visualization.arrayToDataTable([\n`;
+
+      header = Object.keys(projects).sort().filter(p => projects[p].total > config.filterMinimalHours);
+      projectNames = header.concat();
+      header.unshift('week');
+      header = header.map(p => `'${p}'`).join(',');
+      html += `[` + header + `],\n`;
+
+      weeks.map(w => {
+        var oneWeek = [];
+        oneWeek.push("'" + w + "'");
+
+        projectNames.map(pn => {
+          var tot = projects[pn][w];
+          if (tot == undefined) tot = 0;
+          oneWeek.push(tot);
+        });
+        oneWeek = oneWeek.join(',');
+        html += `[` + oneWeek + `],\n`;
+      });
+
+      html += `              
+            ]);
+    
+            var options = {
+              title: 'WakaTime projects',
+              hAxis: {title: 'Week',  titleTextStyle: {color: '#333'}},
+              vAxis: {minValue: 0},
+              isStacked: true
+            };
+    
+            var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+            chart.draw(data, options);
+          }
+        </script>
+      </head>
+      <body>
+        <div id="chart_div" style="width: 100%; height: 500px;"></div>
+      </body>
+    </html>`;
+      fs.writeFileSync("chart.html", html, {flag:"w"});
+      console.log("saved results to chart.html");
+      process.exit();
+    });
+  });
+});
